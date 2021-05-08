da2d66857638fcf6fb94c8827b2daa17871e57cc
rupaliniture <83824402+rupaliniture@users.noreply.github.com>
2021-05-08 23:13:54 +0530

    Add files via upload

---
+++ Name1.java
@@ -0,0 +1,22 @@
+class Name
+{
+public void display()
+{
+System.out.println("hello Cdac"); 
+ }
+ public void show()
+{
+System.out.println("hello Cdac Mumbai"); 
+ }
+}
+class Name1{
+public static void main(String[]args)
+{
+Name m=new Name();
+m.display();
+Name m1=new Name();
+m1.show();
+}
+}
+
+
